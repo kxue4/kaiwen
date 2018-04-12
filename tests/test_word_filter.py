7285d9c9ab371893d2b53ec61f601c453fcdbd22
kxue4 <kxue4@stevens.edu>
2018-04-11 21:04:05 -0400

    update .travis.yml

---
+++ tests/test_word_filter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 4/11/18 20:33
+# @Author  : Kaiwen Xue
+# @File    : test_word_filter.py
+# @Software: PyCharm
+import unittest
+from unittest import mock
+from word_filter import word_replace
+
+
+class TestWordFilter(unittest.TestCase):
+    @mock.patch('word_filter.get_user_input')
+    def test_word_filter(self, mock_get_user_input):
+        mock_get_user_input.return_value = 'Holy shit! What the fuck?'
+        self.assertEqual(word_replace(), 'Holy ****! What the ****?')
+
+
+if __name__ == '__main__':
+    unittest.main(exit=False, verbosity=2)
